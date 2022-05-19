603a88d04f2cb085f500d918c1d479669d9e2ee9
Zuber Khan <Zuberkhan034@gmail.com>
2022-05-19 11:40:31 +0530

    URL SHORTNER PROJECT COMPLETED

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -10,10 +10,10 @@ const shortUrl = async function (req, res) {
         const longUrl = data.longUrl;
         const BASE_URL = 'http://localhost:3000'
 
-        if (!isValidReqBody(data)){
+        if (!isValidReqBody(data)) {
             return res.status(400).send({ status: false, message: "Please enter longUrl!" })
         }
-            
+
         if (!isValid(longUrl)) {
             return res.status(400).send({ status: false, message: "Please enter longurl valid value!" })
         }
@@ -24,12 +24,12 @@ const shortUrl = async function (req, res) {
 
         const FoundedDataInCache = await GET_ASYNC(`${longUrl}`)
         if (FoundedDataInCache) {
-            return res.status(201).send({ status: true, data: JSON.parse(FoundedDataInCache) })
+            return res.status(200).send({ status: true, msg: 'From Radis', data: JSON.parse(FoundedDataInCache) })
         }
 
-        const isPresentUrl = await urlModel.findOne({longUrl:longUrl}).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
+        const isPresentUrl = await urlModel.findOne({ longUrl: longUrl }).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
         if (isPresentUrl) {
-            return res.status(201).send({ status: true, data: isPresentUrl });
+            return res.status(200).send({ status: true, msg: 'From dDatabase', data: isPresentUrl });
         }
 
         const urlCode = generateCode(7).trim().toLowerCase();
@@ -40,7 +40,7 @@ const shortUrl = async function (req, res) {
         const filteredData = await urlModel.findOne(createdData).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
 
 
-        await SET_ASYNC(`${filteredData.longUrl}`, JSON.stringify(filteredData))    
+        await SET_ASYNC(`${filteredData.longUrl}`, JSON.stringify(filteredData))
 
         res.status(201).send({ status: true, data: filteredData })
 
@@ -53,7 +53,7 @@ const shortUrl = async function (req, res) {
 // =========================================getUrl=======================================================
 
 const getUrl = async function (req, res) {
-    
+
     try {
 
         const urlCode = req.params.urlCode;
@@ -63,6 +63,7 @@ const getUrl = async function (req, res) {
 
         const OriginalUrlInCache = await GET_ASYNC(`${urlCode}`);
         if (OriginalUrlInCache) {
+            console.log('FromRedis')
             return res.status(302).redirect(OriginalUrlInCache);
         }
 
@@ -71,9 +72,11 @@ const getUrl = async function (req, res) {
             return res.status(404).send({ status: false, message: "Url not found" })
         }
 
+        console.log('FromDatabase')
+
         const OriginalUrl = foundedUrl.longUrl;
 
-        await SET_ASYNC(`${foundedUrl.urlCode}`,foundedUrl.longUrl )    
+        await SET_ASYNC(`${foundedUrl.urlCode}`, foundedUrl.longUrl)
 
         return res.status(302).redirect(OriginalUrl)
 
