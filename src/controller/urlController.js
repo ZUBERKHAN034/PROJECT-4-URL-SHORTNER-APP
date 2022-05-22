24b87f86d94f37a387b8331872e5293159badb37
Zuber Khan <Zuberkhan034@gmail.com>
2022-05-22 19:21:11 +0530

    URL SHORTNER PROJECT COMPLETED

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,10 +2,11 @@ const urlModel = require("../models/urlModel");
 const { isValid, isValidReqBody, isValidURL, generateCode } = require('../validator/validation')
 const { SET_ASYNC, GET_ASYNC } = require('../redis/redisConnection');
 
-// =========================================postUrl=======================================================
+///////////////// [ SHORT-URL HANDLER ] /////////////////
 const shortUrl = async function (req, res) {
 
     try {
+        
         const data = req.body;
         const longUrl = data.longUrl;
         const BASE_URL = 'http://localhost:3000'
@@ -29,18 +30,17 @@ const shortUrl = async function (req, res) {
 
         const isPresentUrl = await urlModel.findOne({ longUrl: longUrl }).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
         if (isPresentUrl) {
-            return res.status(200).send({ status: true, msg: 'From dDatabase', data: isPresentUrl });
+            return res.status(200).send({ status: true, msg: 'From Database', data: isPresentUrl });
         }
 
-        const urlCode = generateCode(7).trim().toLowerCase();
+        const urlCode = generateCode(7).toLowerCase();
         const shortUrl = `${BASE_URL}/${urlCode}`
-
 
         const createdData = await urlModel.create({ urlCode, longUrl, shortUrl });
         const filteredData = await urlModel.findOne(createdData).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
 
-
         await SET_ASYNC(`${filteredData.longUrl}`, JSON.stringify(filteredData))
+        await SET_ASYNC(`${filteredData.urlCode}`, filteredData.longUrl)
 
         res.status(201).send({ status: true, data: filteredData })
 
@@ -50,8 +50,7 @@ const shortUrl = async function (req, res) {
 }
 
 
-// =========================================getUrl=======================================================
-
+///////////////// [ GET-URL HANDLER ] /////////////////
 const getUrl = async function (req, res) {
 
     try {
@@ -61,10 +60,10 @@ const getUrl = async function (req, res) {
             return res.status(400).send({ status: false, message: "Only 7 digits or letters urlCode is allowed!" });
         }
 
-        const OriginalUrlInCache = await GET_ASYNC(`${urlCode}`);
-        if (OriginalUrlInCache) {
+        const longUrlInCache = await GET_ASYNC(`${urlCode}`);
+        if (longUrlInCache) {
             console.log('FromRedis')
-            return res.status(302).redirect(OriginalUrlInCache);
+            return res.status(302).redirect(longUrlInCache);
         }
 
         const foundedUrl = await urlModel.findOne({ urlCode: urlCode })
@@ -74,11 +73,9 @@ const getUrl = async function (req, res) {
 
         console.log('FromDatabase')
 
-        const OriginalUrl = foundedUrl.longUrl;
-
         await SET_ASYNC(`${foundedUrl.urlCode}`, foundedUrl.longUrl)
 
-        return res.status(302).redirect(OriginalUrl)
+        return res.status(302).redirect(foundedUrl.longUrl)
 
     } catch (err) {
         return res.status(500).send({ status: false, error: err.message })
--- src/validator/validation.js
+++ src/validator/validation.js
@@ -23,7 +23,7 @@ function generateCode(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
 
-    let result = ' ';
+    let result = '';
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
